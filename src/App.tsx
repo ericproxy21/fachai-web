b035b05cc25ae9103e98fe37635f21d808df9bc1
Eric Li <eric.li@optimetriks.com>
2024-05-05 11:43:10 +0000

    react-ga4

--- src/App.tsx
+++ src/App.tsx
@@ -3,13 +3,13 @@ import React, { useEffect } from "react";
 import "./App.css";
 import Chatbox from "./Components/Chatbox";
 import { helloWorld } from "./api/fachai";
-import ReactGA from "react-ga";
+import ReactGA from "react-ga4";
 ReactGA.initialize(process.env.REACT_APP_GOOGLE_ANALYTICS_ID ?? "G-6T9K6B19XE");
 
 function App() {
-  useEffect(() => {
-    ReactGA.pageview("home");
-  }, []);
+  // useEffect(() => {
+  //   ReactGA.pageview("/");
+  // }, []);
 
   useEffect(() => {
     const fetchData = async () => {
